d7824832de29df31101da152cfd698f9fc9703b5
Edward Hope-Morley <edward.hope-morley@canonical.com>
2019-11-21 17:20:06 -0800

    Add missing jq dep

--- scripts/get-simplestreams-image-converter.sh
+++ scripts/get-simplestreams-image-converter.sh
@@ -1,6 +1,7 @@
 fname=simplestreams-image-convert.sh
 path=/usr/share/glance-simplestreams-sync/$fname
 if ! [ -r "$path" ]; then
+sudo apt install -y jq
 url=`curl https://api.github.com/users/dosaboy/gists 2>/dev/null| jq -r .[].files[].raw_url| grep $fname`
 wget $url -O $path
 fi
